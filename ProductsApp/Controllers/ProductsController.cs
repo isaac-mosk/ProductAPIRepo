60766bd7418a5e6dc0e7d57c8ad33dc4d2a3f181
isaac-mosk <tuo08047@temple.edu>
2026-04-14 15:50:36 -0400

    initial commit

--- ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/Controllers/ProductsController.cs
@@ -12,7 +12,7 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 1, Name = "Man", Category = "Human", Price = 3 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
         };
